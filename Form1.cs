f49ca7c2213d5b8012bbbdbe88484449ca3367fe
guiiivs <39381067+guiiivs@users.noreply.github.com>
2018-10-30 22:37:28 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox2.Text == "") {
+                label8.Text = "Obrigatório*";
+            }
+            //else
+            //{
+            //    label8.Text = "";
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Pessoa candidato = new Pessoa();
+            
+
+                MessageBox.Show("Yay! Dado cadastrado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Erro, senta e chora");
+            }
+        }
+
+   
+
+    }
+}
---
+++ Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Pessoa
+    {
+        private string Nome;
+        private int idade;
+        private char sexo;
+        private string endereco;
+        private string email;
+        private int telefone;
+        private string competencias;
+
+        public string getNome() {
+            return (Nome);
+        }
+
+        public void setNome(string n)
+        {
+            this.Nome = n;
+
+        }
+
+
+        public bool cadastrar() {
+            if {
+
+                
+
+            return true;
+            }
+            else{
+            return false;
+            }
+
+
+        }
+    }
+}
